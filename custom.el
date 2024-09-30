fae9e49e7e4cac700634a59377126fb2611e1d13
Yuriy Bereza <ybereza@gmail.com>
2024-09-30 22:25:27 +0300

    move selected pacakges into custom.el

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(lazy-ruff pyenv-mode lsp-mode go-mode)))
  '(custom-enabled-themes '(spacemacs-dark))
  '(custom-safe-themes
    '("7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf" default)))
