05677fa92e95308db3e875fdd3ed65439a709366
Yury Bereza <ybereza@gmail.com>
2025-01-07 12:08:09 +0300

    add go-mode package

--- rc/emacs-rc-go.el
+++ rc/emacs-rc-go.el
@@ -1,9 +1,12 @@
 ;; emacs-rc-go.el ---
 ;; Copyright (c) 2024 Yury Bereza
 
+(autoload 'go-mode "go-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
+
 (defun my-go-mode-hook()
   (setq indent-tabs-mode t)
   (setq tab-width 4))
 
 (add-hook 'go-mode-hook 'my-go-mode-hook)
-(add-hook 'go-mode-hook 'lsp-deferred)
+;; (add-hook 'go-mode-hook 'lsp-deferred)
