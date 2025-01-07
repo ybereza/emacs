fbd714a50e29635c90f48b7f78f9dd0f7998ade6
Yury Bereza <ybereza@gmail.com>
2025-01-07 12:07:35 +0300

    update custom.el

--- custom.el
+++ custom.el
@@ -3,14 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(modus-operandi))
  '(package-selected-packages '(lazy-ruff pyenv-mode lsp-mode go-mode)))
- '(custom-enabled-themes '(spacemacs-dark))
- '(custom-safe-themes
-   '("7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(put 'dired-find-alternate-file 'disabled nil)
