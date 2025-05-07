16db656476ac9e81ad0852099412e6be4b83f582
Yury Bereza <ybereza@gmail.com>
2025-05-07 11:05:34 +0300

    simpleclip wayland support

--- packages/simpleclip/simpleclip.el
+++ packages/simpleclip/simpleclip.el
@@ -1,3 +1,4 @@
+;;; -*- lexical-binding: t; -*-
 ;;; simpleclip.el --- Simplified access to the system clipboard
 ;;
 ;; Copyright (c) 2012-2020 Roland Walker
@@ -325,6 +326,7 @@ in GNU Emacs 24.1 or higher."
        ((and (fboundp 'window-system) (window-system)
         (or
           (and (boundp 'simpleclip-custom-content-provider)
+               simpleclip-custom-content-provider
             (shell-command-to-string simpleclip-custom-content-provider))
           (and (fboundp 'ns-get-pasteboard)
             (ns-get-pasteboard))
@@ -359,7 +361,9 @@ in GNU Emacs 24.1 or higher."
           ((memq system-type '(gnu gnu/linux gnu/kfreebsd))
            (with-output-to-string
              (with-current-buffer standard-output
-               (call-process "xsel" nil t nil "--clipboard" "--output"))))
+               (if (string= (getenv "XDG_SESSION_TYPE") "wayland")
+                   (call-process "wl-paste" nil t nil)
+                 (call-process "xsel" nil t nil "--clipboard" "--output")))))
           (t
            (error "Clipboard support not available")))
        (error
@@ -399,7 +403,9 @@ in GNU Emacs 24.1 or higher."
            ((memq system-type '(gnu gnu/linux gnu/kfreebsd))
             (with-temp-buffer
               (insert str-val)
-              (call-process-region (point-min) (point-max) "xsel" nil nil nil "--clipboard" "--input")))
+               (if (string= (getenv "XDG_SESSION_TYPE") "wayland")
+                  (call-process "wl-copy" nil t nil)
+                (call-process-region (point-min) (point-max) "xsel" nil nil nil "--clipboard" "--input"))))
            (t
             (error "Clipboard support not available")))
        (error
